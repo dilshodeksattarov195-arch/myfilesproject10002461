e3c6d1a215b1dc068b492679f53b00a123493671
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 14:31:57 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class tokenPenderWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = tokenPenderWorker()
+    print("Worker engine initialized.")
